b5d974a15e4eff1dc0a5705b6ada46bb38cd6200
Anirudh Suresh <anirudhsuresh522001@gmail.com>
2021-05-15 11:34:48 +0530

    Init

--- src/ApolloProvider.js
+++ src/ApolloProvider.js
@@ -8,7 +8,7 @@ import {setContext} from "apollo-link-context"
 
 
 const httpLink=createHttpLink({
-    uri:"http://localhost:5000"
+    uri:"https://morning-woodland-68983.herokuapp.com/"
 })
 
 const authLink=setContext(()=>{
